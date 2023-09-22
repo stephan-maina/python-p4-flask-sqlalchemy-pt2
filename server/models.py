71e6b2105755020c7647151707f2470bbb01b7bf
Mr.Perfect 092 <135037455+stephan-maina@users.noreply.github.com>
2023-09-22 23:57:23 +0300

    Done.

--- server/models.py
+++ server/models.py
@@ -1,31 +1,12 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import MetaData
 
-metadata = MetaData(naming_convention={
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-})
+db = SQLAlchemy()
 
-db = SQLAlchemy(metadata=metadata)
-
-class Owner(db.Model):
-    __tablename__ = 'owners'
-
+class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, unique=True)
+    title = db.Column(db.String(128), nullable=False)
+    author = db.Column(db.String(64), nullable=False)
 
-    pets = db.relationship('Pet', backref='owner')
-
-    def __repr__(self):
-        return f'<Pet Owner {self.name}>'
-
-class Pet(db.Model):
-    __tablename__ = 'pets'
-    
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    species = db.Column(db.String)
-
-    owner_id = db.Column(db.Integer, db.ForeignKey('owners.id'))
-
-    def __repr__(self):
-        return f'<Pet {self.name}, {self.species}>'
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
