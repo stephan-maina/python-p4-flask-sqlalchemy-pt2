dd2e629f41fdae8ea13246047bda0dec8b1ed113
Mr.Perfect 092 <135037455+stephan-maina@users.noreply.github.com>
2023-09-22 23:58:19 +0300

    Done.

--- server/seed.py
+++ server/seed.py
@@ -1,8 +1,18 @@
 #!/usr/bin/env python3
 
-from random import choice as rc
+from app import db
+from models import Book
 
-from faker import Faker
+# Create and add some initial books to the database
+if __name__ == '__main__':
+    db.create_all()
 
-from app import app
-from models import db, Owner, Pet
+    book1 = Book(title='Hakuna Matata', author='Mark')
+    book2 = Book(title='Ghost of Garbatula', author='Keren')
+    book3 = Book(title='CR7 the G.O.A.T', author='Bat-tziyon')
+
+    db.session.add(book1)
+    db.session.add(book2)
+    db.session.add(book3)
+
+    db.session.commit()
